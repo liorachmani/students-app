c81fe98da65d51fb9f08d5749ea436aa92699f9a
Ido Winkler <idowin04@gmail.com>
2025-01-18 18:14:45 +0200

    cr fix

--- app/src/main/java/com/example/mystudentsapplication/AddStudentActivity.kt
+++ app/src/main/java/com/example/mystudentsapplication/AddStudentActivity.kt
@@ -1,10 +1,8 @@
 package com.example.mystudentsapplication
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,27 +12,6 @@ import com.example.mystudentsapplication.model.Model
 import com.example.mystudentsapplication.model.Student
 
 class AddStudentActivity : AppCompatActivity() {
-    private fun onSaveClicked() {
-        val nameTextView: TextView = findViewById(R.id.add_student_activity_username_edit_text)
-        val idTextView: TextView = findViewById(R.id.add_student_activity_id_edit_text)
-        val phoneTextView: TextView = findViewById(R.id.add_student_activity_phone_edit_text)
-        val addressTextView: TextView = findViewById(R.id.add_student_activity_address_edit_text)
-        val checkedCheckbox: CheckBox = findViewById(R.id.add_student_activity_checked_checkbox)
-
-        val student = Student(
-            id = idTextView.text?.toString() ?: "",
-            fullname = nameTextView.text?.toString() ?: "",
-            phone = phoneTextView.text?.toString() ?: "",
-            address = addressTextView.text?.toString() ?: "",
-            isChecked = checkedCheckbox.isChecked
-        )
-
-        Model.shared.addStudent(student) {
-            finish()
-        }
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -54,6 +31,26 @@ class AddStudentActivity : AppCompatActivity() {
 
 
         cancelButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun onSaveClicked() {
+        val nameTextView: TextView = findViewById(R.id.add_student_activity_username_edit_text)
+        val idTextView: TextView = findViewById(R.id.add_student_activity_id_edit_text)
+        val phoneTextView: TextView = findViewById(R.id.add_student_activity_phone_edit_text)
+        val addressTextView: TextView = findViewById(R.id.add_student_activity_address_edit_text)
+        val checkedCheckbox: CheckBox = findViewById(R.id.add_student_activity_checked_checkbox)
+
+        val student = Student(
+            id = idTextView.text?.toString() ?: "",
+            fullname = nameTextView.text?.toString() ?: "",
+            phone = phoneTextView.text?.toString() ?: "",
+            address = addressTextView.text?.toString() ?: "",
+            isChecked = checkedCheckbox.isChecked
+        )
+
+        Model.shared.addStudent(student) {
             finish()
         }
     }
